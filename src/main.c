e3951c7281a91768fcca0f971260dbdd4d98083b
Tobias Pettersson <a.tobias.pettersson@gmail.com>
2017-05-19 11:51:02 +0200

    Fixed output to the console via UART

--- src/main.c
+++ src/main.c
@@ -268,6 +268,20 @@ void uartSetup(void)
 }
 
 /**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART3 and Loop until the end of transmission */
+  HAL_UART_Transmit(&UartHandle, (uint8_t *)&ch, 1, 0xFFFF);
+
+  return ch;
+}
+
+/**
   * @brief  This function is executed in case of error occurrence.
   * @param  None
   * @retval None
