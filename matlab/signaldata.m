0759be08329edb61e0a624383aa6cbad624e4a36
Tobias Pettersson <a.tobias.pettersson@gmail.com>
2017-05-25 16:06:31 +0200

    Started making a matlab-function to generate a test signal/data

---
+++ matlab/signaldata.m
@@ -0,0 +1,36 @@
+function [inputSignal] = signaldata
+% This should generate:
+%       A 5,8 GHz base signal
+%       A 1 kHz base signal
+%       A 500 Hz base signal
+%       A heartbeat signal 100 beats/min
+%       A respiration rate of 16 breaths/min
+%       And then mix these signals together
+
+% Some defines
+HEARTBEATS = 100;
+RESP_RATE = 16;
+BASE_SIGNAL_500HZ = 500;
+%BASE_SIGNAL_1KHZ = 1000;
+%BASE_SIGNAL_5_8_GHZ = 5800000000;
+
+% Calculating the frequency of the heartbeat
+beatsSec =  HEARTBEATS / 60;
+
+% Calculating the frequency of respiration
+respSec = RESP_RATE / 60;
+
+% Samples per second
+fs = 2000;
+timePeriod = 1 / fs;
+
+% I want a dataset for 20 sec
+timeBase = 20;
+myLen = timeBase / timePeriod;
+t = (0:myLen-1) * timePeriod;
+
+% Generation of the signal (not using 5,8 GHz signal atm)
+inputSignal = 0.3*sin(2*pi*beatsSec*t) + 0.7*sin(2*pi*respSec*t) + 0.5*sin(2*pi*BASE_SIGNAL_500HZ*t);
+
+plot(t,inputSignal)
+end
