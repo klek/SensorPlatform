8a95130917204c8c6d316f594349c02f4dfa4a5d
Tobias Pettersson <a.tobias.pettersson@gmail.com>
2017-05-28 14:48:15 +0200

    Added FFT in the matlab example

--- matlab/signaldata.m
+++ matlab/signaldata.m
@@ -1,4 +1,4 @@
-function [inputSignal] = signaldata
+function [inputSignal, beatsSec, respSec] = signaldata
 % This should generate:
 %       A 5,8 GHz base signal
 %       A 1 kHz base signal
@@ -30,7 +30,16 @@ myLen = timeBase / timePeriod;
 t = (0:myLen-1) * timePeriod;
 
 % Generation of the signal (not using 5,8 GHz signal atm)
-inputSignal = 0.3*sin(2*pi*beatsSec*t) + 0.7*sin(2*pi*respSec*t) + 0.5*sin(2*pi*BASE_SIGNAL_500HZ*t);
+inputSignal =  0.3*sin(2*pi*beatsSec*t) + 0.7*sin(2*pi*respSec*t) + 0.5*sin(2*pi*BASE_SIGNAL_500HZ*t);
 
 plot(t,inputSignal)
+
+% FFT
+Y = fft(inputSignal);
+
+P2 = abs(Y/myLen);
+P1 = P2(1:myLen/2+1);
+P1(2:end-1) = 2*P1(2:end-1)
+f = fs * (0:(myLen/2))/myLen;
+plot(f,P1)
 end
